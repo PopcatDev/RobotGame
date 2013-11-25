cc47eb9c13616a3e99de8b52b92dc19b875af894
PopcatDev <Hopkins@148-100-135-227.foxnet.marist.edu>
2013-11-25 15:00:38 -0500

    v1.01 forgot to add movement to old location

--- RunawayBot.py
+++ RunawayBot.py
@@ -1,22 +1,28 @@
+#Evan Hopkins
+#Runaway Bot
+#v1.01
 import rg
 import random
 
 class Robot:
-	#Runaway Bot
     def act(self, game):
         for loc, bot in game.robots.iteritems():
             if bot.player_id != self.player_id:
                 if rg.dist(loc, self.location) <= 1:
                     return ['attack', loc]
 
-        newLoc = randomLocation()
+        newLoc = randomLocation(self.location)
         return ['move', rg.toward(newLoc)]
 
-    def randomLocation(self):
+    def randomLocation(self, location):
     	x = random.randint(-1,1)
     	y = random.randint(-1,1)
+
+    	x = x + location[0]
+    	y = y + location[1]
+
     	if (x==y):
-    		newLoc randomLocation()
+    		newLoc randomLocation(location)
     	else:
     		newLoc = (x,y)
     	return newLoc
